5906dfe89208e2bc4307c965ecd0bc5a42e22f46
novel bafagih <novelbafagih@student.ub.ac.id>
2023-05-19 14:43:10 +0700

    [ADD] Project Proposal - Create fragment proposal - Add edit text start Icon Tint

---
+++ app/src/main/java/com/example/breel/ui/fragment/project/proposal/SubmitProposalFragment.kt
@@ -0,0 +1,28 @@
+package com.example.breel.ui.fragment.project.proposal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.breel.R
+import com.example.breel.databinding.FragmentSubmitProposalBinding
+
+class SubmitProposalFragment : Fragment() {
+    private var _binding: FragmentSubmitProposalBinding? = null
+    private val binding:FragmentSubmitProposalBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSubmitProposalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
