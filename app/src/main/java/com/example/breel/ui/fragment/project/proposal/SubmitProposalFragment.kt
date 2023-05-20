93d959d7e9d16391a89551369c91cfba51674037
novel bafagih <novelbafagih@student.ub.ac.id>
2023-05-20 18:10:41 +0700

    [ADD] Status Bar - trying statusbar in fragment_project_detail.xml and fragment_submit_proposal.xml - Change theme.xml

--- app/src/main/java/com/example/breel/ui/fragment/project/proposal/SubmitProposalFragment.kt
+++ app/src/main/java/com/example/breel/ui/fragment/project/proposal/SubmitProposalFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.breel.R
 import com.example.breel.databinding.FragmentSubmitProposalBinding
 
 class SubmitProposalFragment : Fragment() {
