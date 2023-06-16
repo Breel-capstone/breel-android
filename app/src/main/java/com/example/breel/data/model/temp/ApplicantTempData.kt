cb7b3e4653a6d7d5e80891d47c9d29c9a639f0ce
Puti Adiva <adiva.puti@gmail.com>
2023-06-16 19:01:37 +0700

    [UPDATE] mentorship project

---
+++ app/src/main/java/com/example/breel/data/model/temp/ApplicantTempData.kt
@@ -0,0 +1,7 @@
+package com.example.breel.data.model.temp
+
+data class ApplicantTempData (
+    val id: Int,
+    val name: String,
+    val skill: String
+)
--- app/src/main/java/com/example/breel/ui/fragment/mentorship/adapter/MentorshipProjectAdapter.kt
+++ app/src/main/java/com/example/breel/ui/fragment/mentorship/adapter/MentorshipProjectAdapter.kt
@@ -3,9 +3,12 @@ package com.example.breel.ui.fragment.mentorship.adapter
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.breel.R
 import com.example.breel.data.model.temp.ProjectTempData
 import com.example.breel.databinding.ItemSimpleProjectBinding
+import com.example.breel.ui.fragment.mentorship.project.MentorshipProjectFragmentDirections
 
 class MentorshipProjectAdapter (private val listProject: List<ProjectTempData>) : RecyclerView.Adapter<MentorshipProjectAdapter.ViewHolder>() {
 
@@ -24,6 +27,9 @@ class MentorshipProjectAdapter (private val listProject: List<ProjectTempData>) 
         Log.i(TAG, "halo from on bind!")
         holder.binding.tvTitle.text = listProject[position].title
         holder.binding.status.text = listProject[position].status
+        holder.itemView.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_mentorshipProjectFragment_to_mentorProjectDetailFragment)
+        )
     }
 
     class ViewHolder(var binding: ItemSimpleProjectBinding) : RecyclerView.ViewHolder(binding.root) {
